114736f4201e2e8771f04b8acb26274ecc0879e5
Esat Yucel <esatyucelsoftware@gmail.com>
2026-05-10 02:49:28 +0300

    AboutDTOs and Mappings finished

---
+++ OnlineEdu.API/Controllers/AboutsController.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Entity;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutsController(IAboutService<About> _aboutService, IMapper _mapper) : ControllerBase
+    {
+    }
+}
---
+++ OnlineEdu.API/Mapping/AboutMapping.cs
@@ -0,0 +1,15 @@
+﻿using OnlineEdu.DTO.DTOs.AboutDTOs;
+using OnlineEdu.Entity;
+using AutoMapper;
+
+namespace OnlineEdu.API.Mapping
+{
+    public class AboutMapping : Profile
+    {
+        public AboutMapping()
+        {
+            CreateMap<CreateAboutDTO, About>().ReverseMap();
+            CreateMap<UpdateAboutDTO, About>().ReverseMap();
+        }
+    }
+}
