485559c434192ec5f8edd19c06e2e81145ea7506
Esat Yucel <esatyucelsoftware@gmail.com>
2026-06-19 14:46:28 +0300

    Blog View Components finished

---
+++ OnlineEdu.WebUI/ViewComponents/Blog/_BlogCategoryList.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.BlogCategoryDTOs;
+
+namespace OnlineEdu.WebUI.ViewComponents.Blog
+{
+    public class _BlogCategoryList : ViewComponent
+    {
+        private readonly HttpClient _client;
+
+        public _BlogCategoryList(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("EduClient");
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categoryList = await _client.GetFromJsonAsync<List<ResultBlogCategoryDTO>>("blogcategories");
+
+            var blogCategories = (from blogCategory in categoryList
+                                  select new BlogCategoryWithCountViewModel
+                                  {
+                                      CategoryName = blogCategory.Name,
+                                      BlogCount = blogCategory.Blogs.Count,
+                                      BlogCategoryId = blogCategory.BlogCategoryId
+                                  }).ToList();
+            return View(blogCategories);
+        }
+    }
+}
---
+++ OnlineEdu.WebUI/ViewComponents/Blog/_BlogRecentBlogs.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.BlogDTOs;
+
+namespace OnlineEdu.WebUI.ViewComponents.Blog
+{
+    public class _BlogRecentBlogs : ViewComponent
+    {
+        private readonly HttpClient _client;
+
+        public _BlogRecentBlogs(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("EduClient");
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultBlogDTO>>("blogs/GetLast4Blogs");
+            return View(values);
+        }
+    }
+}
