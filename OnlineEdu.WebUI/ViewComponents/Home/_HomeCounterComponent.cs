a1abdf221589f72522f51269308ee0cb4c996fb5
Esat Yucel <esatyucelsoftware@gmail.com>
2026-07-17 13:54:29 +0300

    Home/Count View Component finished

---
+++ OnlineEdu.WebUI/ViewComponents/Home/_HomeCounterComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.ViewComponents.Home
+{
+    public class _HomeCounterComponent : ViewComponent
+    {
+        private readonly HttpClient _client;
+
+        public _HomeCounterComponent(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("EduClient");
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.blogCount = await _client.GetFromJsonAsync<int>("blogs/GetBlogCount");
+            ViewBag.courseCount = await _client.GetFromJsonAsync<int>("courses/GetCourseCount");
+            ViewBag.courseCategoryCount = await _client.GetFromJsonAsync<int>("courseCategories/GetCourseCategoryCount");
+            ViewBag.testimonialCount = await _client.GetFromJsonAsync<int>("testimonials/GetTestimonialCount");
+            return View();
+        }
+    }
+}
