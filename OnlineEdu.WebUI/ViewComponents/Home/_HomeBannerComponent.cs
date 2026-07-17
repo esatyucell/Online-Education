644b8d2143e9af249694e03daf6f5e0b66238d94
Esat Yucel <esatyucelsoftware@gmail.com>
2026-07-17 13:41:09 +0300

    Home/Banner View Component finished

---
+++ OnlineEdu.WebUI/ViewComponents/Home/_HomeBannerComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.BannerDTOs;
+
+namespace OnlineEdu.WebUI.ViewComponents.Home
+{
+    public class _HomeBannerComponent : ViewComponent
+    {
+        private readonly HttpClient _client;
+
+        public _HomeBannerComponent (IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("EduClient");
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultBannerDTO>>("banners");
+            return View(values);
+        }
+    }
+}
