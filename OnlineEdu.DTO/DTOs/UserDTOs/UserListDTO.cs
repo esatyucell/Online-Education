092dc2563a2921026fc364905d035a166e852884
Esat Yucel <esatyucelsoftware@gmail.com>
2026-09-03 10:17:22 +0300

    API/RoleAssignsController

---
+++ OnlineEdu.DTO/DTOs/UserDTOs/UserListDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DTO.DTOs.UserDTOs
+{
+    public class UserListDTO
+    {
+        public int Id { get; set; }
+        public string NameSurname { get; set; }
+        public string UserName { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
