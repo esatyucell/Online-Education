4dc57c5e4036529c67379e19dd281ec04725ef46
Esat Yucel <esatyucelsoftware@gmail.com>
2026-09-03 14:32:22 +0300

    Business/JwtService Concrete finished

---
+++ OnlineEdu.API/Controllers/RolesController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineEdu.DTO.DTOs.RoleDTOs;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+    }
+}
