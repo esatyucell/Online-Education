e907e2c901e2752e456faa57f06561a1aff71c2a
Esat Yucel <esatyucelsoftware@gmail.com>
2026-09-08 11:40:31 +0300

    API/TeacherSocialsController Finished

--- OnlineEdu.API/Controllers/TeacherSocialsController.cs
+++ OnlineEdu.API/Controllers/TeacherSocialsController.cs
@@ -1,11 +1,56 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.TeacherSocialDTOs;
+using OnlineEdu.Entity.Entities;
 
 namespace OnlineEdu.API.Controllers
 {
+    [Authorize(Roles = "Admin, Teacher")]
     [Route("api/[controller]")]
     [ApiController]
-    public class TeacherSocialsController : ControllerBase
+    public class TeacherSocialsController(IGenericService<TeacherSocial> _teacherSocialService,IMapper _mapper ) : ControllerBase
     {
+        [AllowAnonymous]
+        [HttpGet("byTeacherId/{id}")]
+        public IActionResult GetSocialByTeacherId(int id)
+        {
+            var values = _teacherSocialService.TGetFilteredList(x => x.TeacherId == id);
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _teacherSocialService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _teacherSocialService.TDelete(id);
+            return Ok("Eğitmen Sosyal Medyası Alanı Silindi");
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(CreateTeacherSocialDTO createTeacherSocialDTO)
+        {
+            var newValue = _mapper.Map<TeacherSocial>(createTeacherSocialDTO);
+            _teacherSocialService.TCreate(newValue);
+            return Ok("Yeni Eğitmen Sosyal Medyası Alanı Oluşturuldu");
+        }
+
+        [HttpPut]
+
+        public IActionResult Update(UpdateTeacherSocialDTO updateTeacherSocialDTO)
+        {
+            var value = _mapper.Map<TeacherSocial>(updateTeacherSocialDTO);
+            _teacherSocialService.TUpdate(value);
+            return Ok("Eğitmen Sosyal Medyası Alanı Güncellendi");
+        }
     }
 }
