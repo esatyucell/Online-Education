5220625d6ef7a6e0b45a6053d22ce404689303eb
Esat Yucel <esatyucelsoftware@gmail.com>
2026-09-08 11:42:09 +0300

    UserMapping Finished

---
+++ OnlineEdu.API/Mapping/UserMappings.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using OnlineEdu.DTO.DTOs.RoleDTOs;
+using OnlineEdu.DTO.DTOs.UserDTOs;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Mapping
+{
+    public class UserMappings: Profile
+    {
+        public UserMappings()
+        {
+            CreateMap<AppUser, UserRegisterDTO>().ReverseMap();
+        
+            CreateMap<AppRole, CreateRoleDTO>().ReverseMap();
+            CreateMap<AppRole, UpdateRoleDTO>().ReverseMap();
+            CreateMap<AppUser, ResultUserDTO>().ReverseMap();
+          
+        }
+    }
+}
