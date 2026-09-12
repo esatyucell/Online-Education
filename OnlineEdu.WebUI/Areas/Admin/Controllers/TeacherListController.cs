fa6f0de2aa51f0feee22dd0c1be95f5c9760423b
Esat Yucel <esatyucelsoftware@gmail.com>
2026-09-12 12:06:16 +0300

    UI/TeacherListController Finished

---
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/TeacherListController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.UserDTOs;
+
+namespace OnlineEdu.WebUI.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class TeacherListController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public TeacherListController(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("EduClient");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var teachers = await _client.GetFromJsonAsync<List<ResultUserDTO>>("users/TeacherList");
+            return View(teachers);
+        }
+    }
+}
